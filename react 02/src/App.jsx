a0762b01abd7c5ca0b164f61b45b9791d76c0273
mahak0505 <mahakb.556@gmail.com>
2026-08-24 11:46:01 +0530

    react 02 added

---
+++ react 02/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  function btnClicked(){
+    console.log('button clicked')
+  }
+  return (
+    <div>
+      <button onClick={btnClicked}>Click Me</button>
+    </div>
+  )
+}
+
+export default App
